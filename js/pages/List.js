a62ab20800b600fd0a715614fabe6355ee72ffdf
blankTM <nexuzagd@gmail.com>
2026-09-10 21:24:49 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,7 +51,7 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">Enjoyment</div>
+                            <div class="type-title-sm">Average Enjoyment</div>
                             <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
                     </ul>
